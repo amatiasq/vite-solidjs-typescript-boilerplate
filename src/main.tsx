b3196262e22d9f6eadb2e105d3254154bdfc7ab0
A. Matías Quezada <amatiasq@gmail.com>
2023-02-22 15:39:13 +0100

    Vite, SolidJS and Typescript boilerplate

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+/* @refresh reload */
+import { render } from 'solid-js/web';
+
+import { App } from './components/App';
+import './index.css';
+
+render(
+  () => <App />,
+  document.getElementById('root') as HTMLElement
+);
